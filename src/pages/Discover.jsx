f9d3a1ccf4d683b24a69eb835f586102db42e983
arif.shellbeehaken <arifkhan.shellbeehaken@gmail.com>
2023-02-13 18:28:37 +0600

    rapidApi integrate and datafetching

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,40 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  console.log(data);
+  const genreTitle = "";
+
+  if (isFetching) return <Loader title="Loading Songs...." />;
+  if (error) return <Error />;
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          # Explore Your Song
+        </h2>
+        <select
+          onClick={() => {}}
+          value=""
+          className="bg-blue-500 text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre, idx) => (
+            <option className="outline-none" value={genre.value} key={idx}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data.tracks?.map((song, ix) => (
+          <SongCard key={song.key} song={song} i={ix} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const shazamApi = createApi({
+    reducerPath: "shazamApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam.p.rapidapi.com',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '3558ab9bd8msh64299fab6d1ea0dp1e8532jsn2008e64a34e6');
+            headers.set('X-RapidAPI-Host','shazam.p.rapidapi.com');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({query: () => "/charts/track"}),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamApi;
